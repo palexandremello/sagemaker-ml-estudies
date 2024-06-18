5b3d0d5c7d2cd3bec4504da2f07efa11d1b632d0
Paulo Alexandre Mello <palexandremello@gmail.com>
2024-06-18 04:46:40 -0300

    chore: to force a deploy

--- src/bert-tokenizer/inference.py
+++ src/bert-tokenizer/inference.py
@@ -54,6 +54,8 @@ def predict_fn(input_data, model: HuggingFaceTokenizer):
     Perform prediction on the input data.
     """
     input_tokens, input_id_tokens = model.predict(input_data["text"])
+
+    
     return {"tokens": input_tokens, "id_tokens": input_id_tokens}
 
 def output_fn(prediction, content_type):
